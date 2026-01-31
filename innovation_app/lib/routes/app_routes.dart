c399354afdc1a9d79644760011d706724163e1b1
Eduardo Silva <eduardo998468@gmail.com>
2026-01-31 20:30:08 -0300

    refactor: remove unused route imports and constants from AppRoutes

--- innovation_app/lib/routes/app_routes.dart
+++ innovation_app/lib/routes/app_routes.dart
@@ -4,10 +4,6 @@ import '../presentation/screens/register_screen.dart';
 import '../presentation/screens/reset_password_screen.dart';
 import '../presentation/screens/dashboard_screen.dart';
 import '../presentation/screens/terms_screen.dart';
-import '../presentation/screens/plans_screen.dart';
-import '../presentation/screens/payment_screen.dart';
-import '../presentation/screens/company_candidates_screen.dart';
-import '../presentation/screens/jobs_manage_screen.dart';
 import '../presentation/screens/document_history_screen.dart';
 
 class AppRoutes {
@@ -16,10 +12,6 @@ class AppRoutes {
   static const resetPassword = '/reset';
   static const dashboard = '/dashboard';
   static const terms = '/terms';
-  static const plans = '/plans';
-  static const payment = '/payment';
-  static const companyCandidates = '/company/candidates';
-  static const jobsManage = '/company/jobs';
   static const documentHistory = '/documents/history';
 
   static Map<String, WidgetBuilder> routes = {
@@ -28,10 +20,6 @@ class AppRoutes {
     resetPassword: (_) => const ResetPasswordScreen(),
     dashboard: (_) => const DashboardScreen(),
     terms: (_) => const TermsScreen(),
-    plans: (_) => const PlansScreen(),
-    payment: (_) => const PaymentScreen(),
-    companyCandidates: (_) => const CompanyCandidatesScreen(),
-    jobsManage: (_) => const JobsManageScreen(),
     documentHistory: (_) => const DocumentHistoryScreen(),
   };
 }
