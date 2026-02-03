353d877434792f4b444cc8e74b99bc254ac9a7a1
Eduardo Silva <eduardo998468@gmail.com>
2026-02-03 20:52:44 -0300

    Limpeza total de índices e submódulos

--- innovation/app/core/plans.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from enum import Enum
-
-
-class PlanFeature(str, Enum):
-    SERVICES_VALIDATION = "services_validation"
-    SERVICES_FULL = "services_full"
-
