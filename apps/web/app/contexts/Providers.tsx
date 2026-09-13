e2f545a08e3993ffd1e3899e10c5854c112af857
Eduardo Silva <eduardo998468@gmail.com>
2026-09-12 23:08:47 -0300

    fix(infra): corrige rota HTTPS da API para evitar Mixed Content Blocked e adiciona Toaster para ver erros

--- apps/web/app/contexts/Providers.tsx
+++ apps/web/app/contexts/Providers.tsx
@@ -2,12 +2,14 @@
 import React, { ReactNode } from 'react';
 import { AuthProvider } from './AuthContext';
 import { LanguageProvider } from './LanguageContext';
+import { Toaster } from 'sonner';
 
 export const Providers: React.FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <LanguageProvider>
       <AuthProvider>
         {children}
+        <Toaster position="top-right" richColors />
       </AuthProvider>
     </LanguageProvider>
   );
